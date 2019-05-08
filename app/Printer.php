5680fe809de919f748bbc5a14118095bc8213e9e
Robert Hutter <hutterrbert@gmail.com>
2019-05-08 08:49:41 +0200

    adatábzis kapcsolatok

--- app/Printer.php
+++ app/Printer.php
@@ -4,6 +4,7 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 use App\Cegek;
+use App\PrinterCounter;
 
 class Printer extends Model
 {
@@ -12,5 +13,9 @@ class Printer extends Model
     public function ceg()
     {
         return $this->belongsTo(Cegek::class, 'ceg_id', 'id');
+    }
+    public function szamlalo()
+    {
+        return $this->hasMany(PrinterCounter::class, 'printer_id', 'id');
     }
 }
--- app/PrinterCounter.php
+++ app/PrinterCounter.php
@@ -3,8 +3,19 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Printer;
 
 class PrinterCounter extends Model
 {
-    //
+    protected $table = 'printer_counters';
+    protected $guarded = [];
+
+    public function nyomtato()
+    {
+        return $this->belongsTo(Printer::class, 'printer_id', 'id');
+    }
+    public function nyomtatoCeg()
+    {
+        return $this->hasOneThrough('App\Cegek', 'App\Printer', 'id', 'id', 'printer_id', 'ceg_id');
+    }
 }
