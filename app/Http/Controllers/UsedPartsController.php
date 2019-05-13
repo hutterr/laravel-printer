20f8648e8841c6cc7265a6b91f34699801b81701
Robert Hutter <hutterrbert@gmail.com>
2019-05-13 15:29:48 +0200

    alkatrész tábla és elhasznált alkatrész tábla

---
+++ app/Http/Controllers/UsedPartsController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\UsedParts;
+use Illuminate\Http\Request;
+
+class UsedPartsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\UsedParts  $usedParts
+     * @return \Illuminate\Http\Response
+     */
+    public function show(UsedParts $usedParts)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\UsedParts  $usedParts
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(UsedParts $usedParts)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\UsedParts  $usedParts
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, UsedParts $usedParts)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\UsedParts  $usedParts
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(UsedParts $usedParts)
+    {
+        //
+    }
+}
---
+++ database/migrations/2019_05_13_130019_create_used_parts_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUsedPartsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('used_parts', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->integer('');
+            $table->unsignedInteger('printer_id')->nullable($value = false);
+            $table->unsignedBigInteger('parts_id')->nullable($value = false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('used_parts');
+    }
+}
